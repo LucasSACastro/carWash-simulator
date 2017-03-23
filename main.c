6d6d21027b4fc815fee473cbdc6b5aac4a2cf67d
LucasSACastro <lucassebastiao2010@live.com>
2017-03-23 02:48:29 -0300

    Working on the algorithm
    Properly simulating the thing.

--- main.c
+++ main.c
@@ -1,11 +1,13 @@
 /*
  * File:   main.c
- * Author: Luke S. A. Castro / Victor Pius
+ * Author: Luke S. A. Castro & Victor Pius
  *
  * Created on 22 de Março de 2017, 14:01
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
 
 #define OUTPUT_FILENAME "output.txt"
 
@@ -19,9 +21,10 @@ void sim(unsigned numero_clientes, FILE *fout);
  ** despois, para retornar 0.
  **/
 int main(){
-
 	unsigned int numero_clientes;
 	FILE *fout;
+
+	setlocale(LC_ALL, "Portuguese");
 
 	printf("Número de clientes para simulação: ");
 	scanf("%d",&numero_clientes);
@@ -55,8 +58,31 @@ void sim(unsigned int numero_clientes,FILE *fout){
 		unsigned instance, sum;
 	} ocio_operador;
 
-	for(cliente=0; cliente<numero_clientes; cliente++){
-		/* Simulação. */
-	}
+    /* Inicialização das variáveis da simulação: */
+    chegada.relogio=servico.fim=servico.sum_duracao=tempo_cliente.sum_fila =
+    tempo_cliente.sum_totais=ocio_operador.sum = 0;
+
+	for(cliente=1; cliente<=numero_clientes; cliente++) {
+        /* Cálculo das estatísticas para este cliente: */
+        srand(rand());
+		chegada.delay = (rand()%3)+10;
+        servico.duracao = (rand()%3)+9;
+        chegada.relogio += chegada.delay;
+        servico.fim>chegada.relogio ? servico.inicio=servico.fim : servico.inicio=chegada.relogio;
+        tempo_cliente.fila = servico.inicio-chegada.relogio;
+        ocio_operador.instance;
+        servico.fim = servico.inicio+servico.duracao;
+        tempo_cliente.total = servico.duracao+tempo_cliente.fila;
+
+        /* Actualizando-se as quattro somas: */
+        servico.sum_duracao      += servico.duracao;
+        tempo_cliente.sum_fila   += tempo_cliente.fila;
+        tempo_cliente.sum_totais += tempo_cliente.totais;
+        ocio_operador.sum        += ocio_operador.instance;
+
+        /* INSIRA AQUI A IMPRESSÃO NA TELA */
+    }
+
+    /* Equações finais: */
 
 }
